a57cbed35ada2c2b0ba9db0380802d794820aa01
juan-math <juan.ftsm50@gmail.com>
2024-10-02 19:58:30 -0300

    Aula 3
    Reorganização de código e adição do exemplo 1.

---
+++ Aula 2/exemplo.py
@@ -0,0 +1,10 @@
+#Esses serão os passos do pirata 
+print("Hola , word")
+# Adicione uma linha dizendo
+print("Este é o caminho para o mapa do tesouro ")
+print("Ola , eu o pirata Steve")
+print("Andar para direita")
+print("Andar para esqueda")
+print("Andar para baixo")
+#e outra frase no final ddo código dizendo 
+print("Proucurar o X e cavar")
---
+++ Aula 3/exemplo1.py
@@ -0,0 +1,4 @@
+print("Vamos calcular o imposto ! coloque o valor do pruduto")
+valorproduto = float(input("Coloque o valor do produto"))
+imposto = valorproduto * 0.1
+print(f"O valor que você terá que pagar  de imposato desse produto é:{imposto}")
