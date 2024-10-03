4a3aaf7a736062ed89e21775e1c4e56350d1aa04
juan-math <juan.ftsm50@gmail.com>
2024-10-02 21:45:45 -0300

    exercicio 3
    Calculamos o tamanho do sapato do cachorro , sapato e coleira

---
+++ Aula 3/exercio3.py
@@ -0,0 +1,11 @@
+# Faça um programa que calcule o tamanho do sapato de um cachorro sabendo que será o dobro do tamanho da coleira dele.
+print(" Óla , boa noite . Obrigado por escolhe a AmericanPET")
+tamanho_cachorro = float(input("Entre com tamanho "))
+sapato= tamanho_cachorro * 2
+print(f" O tamanho do sapato do seu cachorro é {sapato}")
+#Calcule o tamanho da fucinheira sabendo que é otriplo do tamanho dp sapato
+coleira = int(input("Entre com valor da coleria do seu cachorro"))
+tamanhosapato = coleira * 2
+tamanhofuincinheira = tamanhosapato * 3
+print(f"Recomenda-se comprar o spato no seguinte tamanho:{tamanhosapato}")
+print(f"Recomenda-se comprar o spato no seguinte tamanho:{tamanhofuincinheira}")
