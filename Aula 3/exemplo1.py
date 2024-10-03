9b2f0870276fc8bbc6cdb3bcccb9253c2b9ee818
juan-math <juan.ftsm50@gmail.com>
2024-10-02 21:26:30 -0300

    exercicio 1
    Calculamos o e IPVA DO CARRO

--- Aula 3/exemplo1.py
+++ Aula 3/exemplo1.py
@@ -2,9 +2,6 @@ print("Vamos calcular o imposto ! coloque o valor do pruduto")
 valorproduto = float(input("Coloque o valor do produto"))
 imposto = valorproduto * 0.1
 print(f"O valor que você terá que pagar  de imposato desse produto é:{imposto}")
-# Faça um programa que calcule o IPVA de um carro com base no valor do mesmo. O ipva é 4 % do valor do carro 
-valorcaro=float(("Por favor , entre com valor do carro "))
-ipva =valorcaro * 4/100
-print(f"o valor do IPVA  de seu carro é {ipva}")
+
 
 
---
+++ Aula 3/exercio1.py
@@ -0,0 +1,5 @@
+# Faça um programa que calcule o IPVA de um carro com base no valor do mesmo. O ipva é 4 % do valor do carro 
+valorcaro=float(("Por favor , entre com valor do carro "))
+ipva =valorcaro * 4/100
+print(f"o valor do IPVA  de seu carro é {ipva}")
+
