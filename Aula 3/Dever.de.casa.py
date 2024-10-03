013ea2598b8a4cbe047b99b27bb96148d95c25c5
juan-math <juan.ftsm50@gmail.com>
2024-10-02 21:54:54 -0300

    Dever de casa
    Trabalho para casa

---
+++ Aula 3/Dever.de.casa.py
@@ -0,0 +1,7 @@
+# Peça para o usuario o tamanho do aro da roda bicicleta que ele quer comprar e com bese nisso :
+
+# Recomende o tamanho do guidão , da manete e dod quadro da bicicleta 
+
+# 1° O tamanho do quidão será do tamanho aro da roda 
+# 2° O tamanho da mente será 1/4 do tamanho do aro da roda 
+#3° O tamanho quadro da bicleta será o mesmo do aro da roda 
